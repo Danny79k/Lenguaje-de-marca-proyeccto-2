4e50a7376bf69c663213886683f16040e01a761f
Danny79k <belloli.danny@gmail.com>
2024-03-11 18:46:16 +0000

    version 2.0

--- javascript/script-index.js
+++ javascript/script-index.js
@@ -53,3 +53,54 @@ function borra(){
 
     document.getElementById("prote-display").innerHTML = ""
 }
+
+function rellenaTabla(){
+
+    
+
+a = prompt("introduce el grupo muscular1")
+b = prompt("introduce el ejercicio1")
+c = prompt("introduce el numero de repeticiones1")
+d = prompt("introduce introduce el ejercicio2")
+e = prompt("introduce el numero de repeticiones2")
+g = prompt("introduce el numero de repeticiones2")
+e = prompt("introduce introduce el ejercicio3")
+f = prompt("introduce el numero de repeticiones3")
+h = prompt("introduce el numero de repeticiones3")
+i = prompt("introduce introduce el ejercicio4")
+l = prompt("introduce el numero de repeticiones4")
+m = prompt("introduce el numero de repeticiones4")
+
+document.getElementById("musculo-custom-1").innerHTML = a
+document.getElementById("musculo-custom-2").innerHTML = b
+document.getElementById("musculo-custom-3").innerHTML = c
+document.getElementById("musculo-custom-4").innerHTML = d
+
+document.getElementById("ejercicio-custom-1").innerHTML = e 
+document.getElementById("ejercicio-custom-2").innerHTML = g
+document.getElementById("ejercicio-custom-3").innerHTML = e
+document.getElementById("ejercicio-custom-4").innerHTML = f
+
+document.getElementById("repeticiones-custom-1").innerHTML = h
+document.getElementById("repeticiones-custom-2").innerHTML = i
+document.getElementById("repeticiones-custom-3").innerHTML = l
+document.getElementById("repeticiones-custom-4").innerHTML = m
+}
+
+function borraTablaCustom(){
+
+    document.getElementById("musculo-custom-1").innerHTML = " "
+    document.getElementById("musculo-custom-2").innerHTML = " "
+    document.getElementById("musculo-custom-3").innerHTML = " "
+    document.getElementById("musculo-custom-4").innerHTML = " "
+
+    document.getElementById("ejercicio-custom-1").innerHTML = " "
+    document.getElementById("ejercicio-custom-2").innerHTML = " "
+    document.getElementById("ejercicio-custom-3").innerHTML = " "
+    document.getElementById("ejercicio-custom-4").innerHTML = " "
+
+    document.getElementById("repeticiones-custom-1").innerHTML = " "
+    document.getElementById("repeticiones-custom-2").innerHTML = " "
+    document.getElementById("repeticiones-custom-3").innerHTML = " "
+    document.getElementById("repeticiones-custom-4").innerHTML = " "
+}
